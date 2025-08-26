9fec5319345899794f77b4197cbd2fb0c6180dfc
ashcc24 <ashwiniraoedu@gmail.com>
2025-08-26 19:06:50 +0530

    Footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div className='text-3xl font-bold underline'>
-      <p>This is hopless opus</p>
-    </div>
-    </>
-  )
+    
+<>
+</>
+  );
 }
 
 export default App
