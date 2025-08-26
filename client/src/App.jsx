8295300e7279062d83b64a3b5a2c2733546b2bb2
pr3v3r <pravarsingh@pravars-MacBook-Air.local>
2025-08-26 18:59:36 +0530

    Added Profile Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Profile from './pages/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <div className='text-3xl font-bold underline'>
-      <p>This is hopless opus</p>
-    </div>
+<Profile></Profile>
     </>
   )
 }
